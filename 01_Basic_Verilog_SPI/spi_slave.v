ed6413e3c42624fae3cc8e3decc53d94e3bf6215
Akhilesh Ibrahimpet <akhileshibrahimpet@gmail.com>
2026-07-20 18:07:28 +0530

    Add basic SPI slave RTL implementation

--- 01_Basic_Verilog_SPI/spi_slave.v
+++ 01_Basic_Verilog_SPI/spi_slave.v
@@ -1,1 +1,12 @@
-
+module spi_slave(
+input sclk, ss_n, mosi,
+output reg [7:0] rx_data);
+reg [2:0] bit_count;
+always @(posedge sclk)
+begin
+if(!ss_n) begin
+rx_data <= {rx_data[6:0], mosi};
+bit_count <= bit_count + 1;
+end
+end
+endmodule
